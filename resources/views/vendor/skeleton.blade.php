cc336f0d96ebaacf072948c3d27695ea7343983b
Hasan Aftab <hasanaftab007@gmail.com>
2024-01-18 21:48:17 +0500

    Modify skeleton bg to glass shade

--- resources/views/vendor/skeleton.blade.php
+++ resources/views/vendor/skeleton.blade.php
@@ -4,15 +4,15 @@
 <div class="flex gap-3">
     @for($column = 0; $totalColumns > $column ; $column++)
         <div class="flex flex-col gap-4 w-72 mt-5 ms-5">
-            <div class="skeleton h-32 w-full"></div>
-            <div class="skeleton h-8 w-full"></div>
-            <div class="skeleton h-8 w-full"></div>
-            <div class="skeleton h-8 w-full"></div>
-            <div class="skeleton h-8 w-full"></div>
-            <div class="skeleton h-8 w-full"></div>
-            <div class="skeleton h-8 w-full"></div>
-            <div class="skeleton h-8 w-full"></div>
+            <div class="skeleton h-32 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
         </div>
     @endfor
-        <div class="skeleton h-8 w-64 mt-5 me-2"></div>
+        <div class="skeleton h-8 w-64 mt-5 me-2 bg-black bg-opacity-50 backdrop-blur-md"></div>
 </div>

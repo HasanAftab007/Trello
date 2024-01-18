6cb85f2a03f533f2e0d88896acfa8c5f7926f98a
Hasan Aftab <hasanaftab007@gmail.com>
2024-01-18 20:31:59 +0500

    Add one more skeleton div for column creation

--- resources/views/vendor/skeleton.blade.php
+++ resources/views/vendor/skeleton.blade.php
@@ -1,7 +1,7 @@
 @php
     $totalColumns = \App\Models\Column::count();
 @endphp
-<div class="flex gap-2">
+<div class="flex gap-3">
     @for($column = 0; $totalColumns > $column ; $column++)
         <div class="flex flex-col gap-4 w-72 mt-5 ms-5">
             <div class="skeleton h-32 w-full"></div>
@@ -14,4 +14,5 @@
             <div class="skeleton h-8 w-full"></div>
         </div>
     @endfor
+        <div class="skeleton h-8 w-64 mt-5 me-2"></div>
 </div>

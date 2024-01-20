01efaa7a11c9f87d802071736929bcf667798e8a
Hasan Aftab <hasanaftab007@gmail.com>
2024-01-20 19:49:10 +0500

    SLight change in skeleton according to column & cards result.

--- resources/views/vendor/skeleton.blade.php
+++ resources/views/vendor/skeleton.blade.php
@@ -4,18 +4,14 @@
 
 <div class="flex gap-3">
 
-    @for($column = 0; $totalColumns > $column ; $column++)
+    @foreach (\App\Models\Column::withCount('cards')->get() as $column)
         <div class="flex flex-col gap-4 w-72 mt-5 ms-5">
             <div class="skeleton h-32 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
-            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
-            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
-            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
-            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
-            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
-            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
-            <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            @for ($cardIndex = 0; $cardIndex < $column->cards_count; $cardIndex++)
+                <div class="skeleton h-8 w-full bg-black bg-opacity-50 backdrop-blur-md"></div>
+            @endfor
         </div>
-    @endfor
+    @endforeach
 
     @if($totalColumns === 0)
         <div class="skeleton h-10 w-64 ms-9 mt-4 bg-black bg-opacity-50 backdrop-blur-md"></div>
